0636c4ea76720fd3125359ec82082d07c73af4ae
Johanna Larsson <johanna.a.m.larsson@gmail.com>
2026-05-31 21:12:35 +0100

    precommit

--- test/support/dummy.ex
+++ test/support/dummy.ex
@@ -1,14 +1,17 @@
 defmodule Dummy.FakeRenderer do
+  @moduledoc false
   def render(title, _desc), do: {:ok, "fake:" <> title}
 end
 
 defmodule Dummy.FakeContent do
+  @moduledoc false
   def content_for("home"), do: {"Home", "Welcome"}
   def content_for("about"), do: {"About", "About me"}
   def content_for(_), do: :error
 end
 
 defmodule Dummy do
+  @moduledoc false
   use OGMate,
     all_keys: ["home", "about"],
     content_for: Dummy.FakeContent,
